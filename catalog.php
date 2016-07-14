15ed08f4acc89737c2b9a54ed9c0b0f603ef1672
willallard <willallard@Will-Allards-MacBook-Pro.local>
2016-07-14 14:22:01 -0400

    Yup

--- catalog.php
+++ catalog.php
@@ -1,5 +1,8 @@
 <?php 
+include("inc/data.php");
+
 $pageTitle = "Full Catalog";
+$section - null;
 
 if (isset($_GET["cat"])) {
     if ($_GET["cat"] == "books") {
@@ -15,10 +18,26 @@ include("inc/header.php"); ?>
       
       <!-- Content -->
       <div class="container">
+        <div class="section catalog page">
           <div class="row">
           <div class="col-md-6 col-md-offset-3">
             <h1 class="text-center"><?php echo $pageTitle; ?></h1>
           </div>
+            <div class="col-md-12">
+                 <ul class="items">
+                     <?php 
+                     foreach($catalog as $item) {
+                        echo "<li><a href='#'><img src='" 
+                        . $item["img"] . "' alt='"  
+                        . $item["title"] . "' />"
+                        . "<p>View Details</p>"
+                        "</a></li>"; 
+                     }
+                     ?>
+                     
+                </ul> 
+            </div>
+            </div>
           </div>
       </div> 
       
